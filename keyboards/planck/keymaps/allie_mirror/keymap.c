71915ec2831f19708c2675cd1f88784382301e27
Allie Stephan <allie@pointguard.dev>
2022-07-17 16:37:44 -0400

    Add allie_mirror planck layout

--- keyboards/planck/keymaps/allie_mirror/keymap.c
+++ keyboards/planck/keymaps/allie_mirror/keymap.c
@@ -24,7 +24,8 @@ enum planck_layers {
   _RAISE,
   _ADJUST,
   _FN,
-  _NUM
+  _NUM,
+  _MIRROR
 };
 
 enum planck_keycodes {
@@ -51,12 +52,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-------------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_W,  KC_E, KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    LGUI_T(KC_ESC), KC_A,    KC_S,  KC_D, KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,        KC_Z,    KC_X,  KC_C, KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    KC_LCTL,        KC_LALT, NUM,   FN,   LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB,         KC_Q,    KC_W,  KC_E, KC_R,    KC_T,    KC_Y,                KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    LGUI_T(KC_ESC), KC_A,    KC_S,  KC_D, KC_F,    KC_G,    KC_H,                KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT,        KC_Z,    KC_X,  KC_C, KC_V,    KC_B,    KC_N,                KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    KC_LCTL,        KC_LALT, NUM,   FN,   LOWER,   KC_SPC,  LT(_MIRROR, KC_SPC), RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
+/* mirror
+ * ,-------------------------------------------------------------------------------------.
+ * |  Tab  |   Q  |   W   |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |-------+------+-------+------+------+------+------+------+------+------+------+------|
+ * |GUI/Esc|   A  |   S   |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |-------+------+-------+------+------+------+------+------+------+------+------+------|
+ * | Shift |   Z  |   X   |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * |-------+------+-------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl  | Alt  |NUM/GUI|  FN  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * `-------------------------------------------------------------------------------------'
+ */
+[_MIRROR] = LAYOUT_planck_grid(
+    KC_BSPC,         KC_P,    KC_O,  KC_I, KC_U,    KC_Y,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_QUOT, KC_SCLN,    KC_L,  KC_K, KC_J,    KC_H,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_ENT,        KC_SLSH,    KC_DOT,  KC_COMM, KC_M,    KC_N,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    KC_LCTL,        KC_LALT, NUM,   FN,   LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+),
 /* FN
  * ,-----------------------------------------------------------------------------------.
  * | PSCR |   Q  |   W  |   E  |   R  |   T  |   Y  | Home | End  | PgUp |PgDown|  Del |
